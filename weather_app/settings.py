06826686286f0d6c0bccfc61d0d8b5ee4bb26153
Nathan Perrier <107982946+nathan-perrier23@users.noreply.github.com>
2023-12-22 05:18:29 +0000

    Update ALLOWED_HOSTS in settings.py #24

--- weather_app/settings.py
+++ weather_app/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = "django-insecure-_@8c=m2aimoga43jzw#h3w4%t)4+s(@guy1&s_v08h_)yjl-o1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [socket.gethostbyname(socket.gethostname()), '127.0.0.1', 'localhost', '192.168.0.178']
+ALLOWED_HOSTS = [socket.gethostbyname(socket.gethostname()), '127.0.0.1', 'localhost', '192.168.68.67', '192.168.0.178']
 
 # Application definition
 
