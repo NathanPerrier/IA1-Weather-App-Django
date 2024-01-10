31808345c67f591e01ea2a8d1e863efe3e4a7f65
nathan-perrier23 <nathanperrier138@gmail.com>
2024-01-11 00:40:20 +1000

    Update CSS and JavaScript files add images #152

--- weather_app/backend/main.py
+++ weather_app/backend/main.py
@@ -9,14 +9,13 @@ from .chatbot.bot.data import BotData
 from .chatbot.models import Message
 
 from .weatherIcon.main import GetWeatherIcon
+from .location.main import GetLocation
 
 @csrf_protect
 def index(request):
-    # BotData().get_user_ip(request)
-    context = {'is_authenticated': request.user.is_authenticated, 'icon': GetWeatherIcon().get_weather_icon()}
     Message.objects.all().delete()
     Message.objects.create(role='system', content='You Are a helpful weather assistant that has access to almost all weather data. you are to answer purely weather based questions. try include figures in your reposnse to justify yor reasoning')
-    return render(request, 'landing.html', context)
+    return render(request, 'landing.html', {'is_authenticated': request.user.is_authenticated, 'icon': GetWeatherIcon().get_weather_icon(), 'location': GetLocation().get_location()})
 
 def radar(request):
     return render(request, 'radar.html', {'tomorrowio_api_key': config("TOMORROWIO_API_KEY"), 'is_authenticated': request.user.is_authenticated, 'mapbox_access_token': config('MAPBOX_ACCESS_TOKEN')}) #'google_maps_api_key': config("GOOGLE_MAPS_API_KEY")
