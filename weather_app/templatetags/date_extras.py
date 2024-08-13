4d714527cebfb845e450109c09ab6cb136deadcf
nathan-perrier23 <nathanperrier138@gmail.com>
2024-08-13 20:22:07 +1000

    Update CSS for search and landing pages

--- weather_app/templatetags/date_extras.py
+++ weather_app/templatetags/date_extras.py
@@ -11,6 +11,9 @@ def add_days(date, days):
     return date + timedelta(days=days)
 
 @register.filter
-def add_day():
-    current_date = datetime.datetime.now(pytz.timezone(GetLocation().get_location().timezone))
-    return current_date + timedelta(days=1)
+def add_day(date):
+    created = datetime.datetime.strptime(date, "%Y-%m-%dT%H:%M:%SZ")
+    # Add 2 days
+    created_date = created + timedelta(days=1)
+    
+    return created_date
